7a3ea52ddad34529a4b9cc4ff10d829a82fe81e6
TheFoppian <38819438+TheFoppian@users.noreply.github.com>
2022-11-28 16:16:39 -0600

    First Person Movement + Chair Mechanics Finished

---
+++ Spaceship Game/Assets/Scripts/ChairHandler.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChairHandler : MonoBehaviour
+{
+    GameObject tooltip;
+
+    GameObject player;
+
+    public bool canSit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tooltip = GameObject.Find("ChairTooltip");
+        tooltip.SetActive(false);
+
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canSit && Input.GetKeyDown("f"))
+        {
+            player.GetComponent<PlayerInfo>().sitting = true;
+            
+            canSit = false;
+        }
+
+        if (player.GetComponent<PlayerInfo>().sitting)
+        {
+            Transform seat = transform.Find("Seat");
+
+            seat.eulerAngles += new Vector3(0, Input.GetAxis("Horizontal"), 0);
+
+            player.transform.position = seat.Find("MoveTo").position;
+            Vector3 target = seat.eulerAngles;
+            target.x = 0;
+            target.y += 90;
+            target.z = 0;
+            player.transform.eulerAngles = target;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            tooltip.SetActive(true);
+            canSit = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            tooltip.SetActive(false);
+            canSit = false;
+        }
+    }
+}
---
+++ Spaceship Game/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 1f;
+    public float gravity = -9.81f;
+
+    public float jumpHeight = 2f;
+
+    Vector3 velocity;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.2f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (!GetComponent<PlayerInfo>().sitting)
+        {
+            handleMovement();
+        }
+        else
+        {
+            controller.detectCollisions = false;
+        }
+    }
+    
+    void handleMovement()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask.value);
+
+        GetComponent<PlayerInfo>().grounded = isGrounded;
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        controller.Move((transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical")) * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+            Debug.Log("Jumped!");
+        }
+
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(new Vector3(0, velocity.y * Time.deltaTime));
+    }
+}
